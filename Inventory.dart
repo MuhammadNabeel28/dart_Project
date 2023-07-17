1b0efc127a9480b4b88fd326e6486e1237b7d305
MuhammadNabeel28 <nabeelmansoori28@gmail.com>
2023-07-18 01:46:16 +0500

    Update Code

--- Inventory.dart
+++ Inventory.dart
@@ -1,46 +1,84 @@
+import 'dart:async';
 import 'dart:io';
 
 String user_Id = '';
+bool success = false;
 
 void main() {
-  String abc = '';
+  bool condation = true;
 
   print("============ Welcome To Smaart Inventory ===========");
 
-  while (abc == '') {
-    stdout.write('Login press to : A');
+  while (condation) {
+  
+  if(!success){
+    print('Login First Press : A');
 
+  }else{
+    print('Add New Item : B');
+    print('Edit Item : C');
+    print('Delete Item : D');
+    print('Purchase Item : E');
+    print('Sale Item : S');
+    print('Exit System : Z');
+
+  }
+  
     String? input_Key = stdin.readLineSync()!;
 
     switch (input_Key) {
       case 'A':
         login();
         break;
+        case'Z':
+        condation = false;
+        print('Program Ended');
+        break;
+
     }
   }
 }
 
 void login() {
   List<Map<String, String>> userCredentials = [
-    {'email': 'nabeel_master789.gmail.com', 'password': 'master123'},
-    {'email': 'asif_user.gmail.com', 'password': 'user123'},
+    {'email': 'nabeel_master789@gmail.com', 'password': 'master123'},
+    {'email': 'asif_user@gmail.com', 'password': 'user123'},
   ];
 
-  String? UserId = stdin.readLineSync()?.trim();
-  String? Password = stdin.readLineSync()?.trim();
+  String? UserId_ = '';
+  String? Password_ = '';
+  
+  print('Enter UserId ');
+  UserId_ = stdin.readLineSync()!.trim();
 
-  for (var item in userCredentials) {
-    if (UserId == item['nabeel_master789.gmail.com'] &&
-        Password == item['master123']) {
-      user_Id == 'Master';
+  print('Enter Password ');
+  Password_ = stdin.readLineSync()!.trim();
+   
+  bool isCondation =false;
+  for(var credentials in userCredentials){
+    if(UserId_ == credentials['email'] && Password_ ==  credentials['password']){
+      isCondation = true;
+      break;
+    }
+    if(credentials['email'] == 'nabeel_master789@gmail.com')
+       user_Id == 'master';
+    else
+      user_Id == 'user';
+  }
 
+  if(isCondation){
+    if(user_Id == 'master'){
+      success = true;
       print("Login with master account Id Successfully!");
-    } else if (UserId == item['asif_user.gmail.com'] &&
-        Password == item['user123']) {
-      user_Id == 'User';
+    }
+    else{
+      success = true;
       print("Login with user account Id Successfully!");
-    } else {
-      print('Invalid credentials. Please try again.\n');
     }
   }
+  else{
+    print('Invalid credentials. Please try again.\n');
+  }
+
+  
 }
